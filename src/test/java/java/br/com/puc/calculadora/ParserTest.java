85ef418b073c9e1155090a0cf658c0bfacfe038e
Klholin <marcelocruvinel19@gmail.com>
2026-05-09 09:58:26 -0300

    Testes junit6 implementados

---
+++ src/test/java/java/br/com/puc/calculadora/ParserTest.java
@@ -0,0 +1,25 @@
+package java.br.com.puc.calculadora;
+
+import br.com.puc.calculadora.Parser;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    Parser parser = new Parser();
+
+    @Test
+    void testExpressaoSimples() {
+        assertEquals(7.0, parser.avaliarExpressao("3 + 4"));
+    }
+
+    @Test
+    void testExpressaoComMultiplicacao() {
+        assertEquals(11.0, parser.avaliarExpressao("3 + 4 * 2"));
+    }
+
+    @Test
+    void testExpressaoInvalida() {
+        assertThrows(IllegalArgumentException.class, () -> parser.avaliarExpressao("3 + * 2"));
+    }
+}
